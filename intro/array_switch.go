e9c24dcbee6b0cbc78d575c3abb2b87aba3156b1
Taiwo Kareem <taiwo.kareem36@gmail.com>
2021-05-20 09:38:32 +0100

    updated comments

--- intro/array_switch.go
+++ intro/array_switch.go
@@ -12,6 +12,11 @@ func indexOf(element int, data []int) int {
 }
 
 func main() {
+	// If the array contains 0 in both rows and
+	// columns, change every row and column the
+	// number falls on to 0
+
+	// multidimentional slice declaration
 	array := [][]int{
 		{1, 1, 0, 1, 1},
 		{1, 1, 1, 1, 1},
@@ -20,9 +25,11 @@ func main() {
 		{0, 1, 1, 1, 1},
 	}
 
+	// slices to store the row and index position
 	rows := []int{}
 	cols := []int{}
 
+	// get te rows and column index of the number 0 we are interested in
 	for i, row := range array {
 		for j, col := range row {
 			if col == 0 {
@@ -33,6 +40,7 @@ func main() {
 		}
 	}
 
+	// do the replacement based on the rows and column index we want
 	for i, row := range array {
 		for j, _ := range row {
 			if indexOf(i, rows) > -1 || indexOf(j, cols) > -1 {
@@ -41,5 +49,6 @@ func main() {
 		}
 	}
 
+	// print result
 	fmt.Println(array)
 }
